a62ab95f238d18b0cbc6cc7d5235dce4fc20011a
Madzia <magdaalenka798@gmail.com>
2024-10-12 14:21:19 +0200

    Swich case instruction updated

--- src/test/java/FillFormTest.java
+++ src/test/java/FillFormTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class FillFormTest {
 
@@ -26,16 +31,28 @@ public class FillFormTest {
                 ChromeOptions chromeOptions = new ChromeOptions();
                 WebDriverManager.chromedriver().setup();
                 chromeOptions.addArguments("--start-maximized");
-                chromeOptions.addArguments("--remote-allow-origins");
-
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--disable-search-engine-choice-screen");
+                chromeOptions.addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
             }
             case "firefox" -> {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
                 WebDriverManager.firefoxdriver().setup();
-
+                firefoxOptions.addArguments("start-maximized");
+                firefoxOptions.addArguments("--remote-allow-origins=*");
+                firefoxOptions.addArguments("--disable-search-engine-choice-screen");
+                firefoxOptions.addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
             }
             case "edge" -> {
+                EdgeOptions edgeOptions = new EdgeOptions();
                 WebDriverManager.edgedriver().setup();
-
+                edgeOptions.addArguments("start-maximized");
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                edgeOptions.addArguments("--disable-search-engine-choice-screen");
+                edgeOptions.addArguments("--headless");
+                driver = new EdgeDriver(edgeOptions);
             }
             default -> {
                 throw new UnsupportedOperationException("Unsupported browser selected");
