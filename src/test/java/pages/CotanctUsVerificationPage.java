f81f326de887a08ddc7a7f867b38e03308ed5a01
Madzia <magdaalenka798@gmail.com>
2024-10-13 11:49:55 +0200

    Create selectors and methods in POM page class

--- src/test/java/pages/CotanctUsVerificationPage.java
+++ src/test/java/pages/CotanctUsVerificationPage.java
@@ -1,9 +1,22 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CotanctUsVerificationPage {
     private Logger logger = LoggerFactory.getLogger(CotanctUsVerificationPage.class);
+
+    @FindBy(css=".alert-success")
+    WebElement successfulMessage;
+
+    public String readSuccessfulMessageFromVerificationPage()
+    {
+        String successfulMessageText = successfulMessage.getText();
+        logger.info("Alert message is: " + successfulMessageText);
+        return successfulMessageText;
+    }
+
 
 }
